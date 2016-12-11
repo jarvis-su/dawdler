6cd451d8f57c81c32063b764c3d26ce47e8fd4b0
JarvisSu <jarvis.su@189.cn>
2016-12-11 21:44:45 +0800

    update java classes for profiler project

--- profiler_test/src/main/java/profiler/jvm/jarvis/btrace/ss3/Test_3001.java
+++ profiler_test/src/main/java/profiler/jvm/jarvis/btrace/ss3/Test_3001.java
@@ -1,13 +1,19 @@
 package profiler.jvm.jarvis.btrace.ss3;
 
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 /**
- * Created by C5023792 on 11/29/2016.
+ * -Xshare:off -javaagent:/Users/Jarvis/external_bin/btrace-bin-1.3.8.3/build/btrace-agent.jar=script=/Users/Jarvis/Documents/workspace/dawdler/profiler_test/target/classes/profiler/jvm/jarvis/btrace/ss3/MemoryScript001.class,debug=true,unsafe=true,dumpClasses=true
  */
 public class Test_3001 {
+
+    private static Logger logger = LogManager.getLogger(Test_3001.class.getName());
 
     private static List<String> list = new ArrayList<>();
 
@@ -18,6 +24,7 @@ public class Test_3001 {
         while (true) {
             int i = random.nextInt(100);
             list.add("String " + Integer.toString(i));
+            logger.log(Level.DEBUG, "new item is " + Integer.toString(i));
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
