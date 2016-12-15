1677efdc253300657c93dcb1cff192f54c2fd032
jarvis-su001 <jarvis_su@126.com>
2016-12-15 13:39:15 +0800

    add temp files

--- sonar_test/src/main/java/sonar/ResourceUtil.java
+++ sonar_test/src/main/java/sonar/ResourceUtil.java
@@ -7,4 +7,17 @@ public class ResourceUtil {
 
     public static void releaseResource(AutoCloseable... closeableResource) {
     }
+
+    public String testCC(int i) {
+        if (i % 2 == 0) {
+            if (i == 1) {
+                return String.valueOf(i + 2);
+            }
+
+            return "1";
+        } else {
+            return String.valueOf(i);
+        }
+
+    }
 }
