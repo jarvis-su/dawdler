956feee0d82850ca0f96dcccafe3998aa022691e
jarvis-su001 <jarvis_su@126.com>
2016-12-19 14:03:13 +0800

    ddd

--- profiler_test/src/main/java/profiler/jvm/zzm/chapter2/DirectMemoryOOM.java
+++ profiler_test/src/main/java/profiler/jvm/zzm/chapter2/DirectMemoryOOM.java
@@ -1,6 +1,6 @@
 package profiler.jvm.zzm.chapter2;
 
-import sun.misc.Unsafe;
+//import sun.misc.Unsafe;
 
 import java.lang.reflect.Field;
 
@@ -14,12 +14,12 @@ public class DirectMemoryOOM {
     private static final int _1MB = 1024 * 1024;
 
     public static void main(String[] args) throws Exception {
-        Field unsafeField = Unsafe.class.getDeclaredFields()[0];
-        unsafeField.setAccessible(true);
-        Unsafe unsafe = (Unsafe) unsafeField.get(null);
-        while (true) {
-            unsafe.allocateMemory(_1MB);
-        }
+//        Field unsafeField = Unsafe.class.getDeclaredFields()[0];
+//        unsafeField.setAccessible(true);
+//        Unsafe unsafe = (Unsafe) unsafeField.get(null);
+//        while (true) {
+//            unsafe.allocateMemory(_1MB);
+//        }
     }
 }
 
