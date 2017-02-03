8ea7dc5165306a669a1909d0c930145fb1effe9b
JarvisSu <jarvis.su@189.cn>
2017-02-03 21:11:02 +0800

    spring boot init

---
+++ sysMgmt/src/main/java/com/jarvis/SysMgmtApplication.java
@@ -0,0 +1,12 @@
+package com.jarvis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysMgmtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SysMgmtApplication.class, args);
+    }
+}
