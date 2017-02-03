0e6be478343cb43880b78d5c26a1ee7057b03f8d
JarvisSu <jarvis.su@189.cn>
2017-02-03 21:41:17 +0800

    spring boot init

---
+++ sysMgmt/src/main/java/com/jarvis/web/SimpleController.java
@@ -0,0 +1,13 @@
+package com.jarvis.web;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class SimpleController {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @ResponseBody
+    public String hello(@RequestParam String name) {
+        return "Hello " + name;
+    }
+}
