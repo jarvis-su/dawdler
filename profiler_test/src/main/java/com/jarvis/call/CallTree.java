a6cd742309d480d6f803416d66e9a77614a2e369
Jarvis Su <jie.su@xerox.com>
2017-02-04 09:03:12 +0800

    add new branch dev

--- profiler_test/src/main/java/com/jarvis/call/CallTree.java
+++ profiler_test/src/main/java/com/jarvis/call/CallTree.java
@@ -3,8 +3,24 @@ package com.jarvis.call;
 public class CallTree {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Before sleep ");
 
+		long duration = getSleepDuration();
+		goToSleep(duration);
+		System.out.println("After sleep ");
+	}
+
+
+	public static long getSleepDuration() {
+		return 20000l;
+	}
+
+	public static void goToSleep(long duration) {
+		try {
+			Thread.sleep(duration);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
