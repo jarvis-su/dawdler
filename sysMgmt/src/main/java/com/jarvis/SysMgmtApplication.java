4b8cc545863e56a3b72f793b09554e6a6e724add
jarvis-su001 <jarvis_su@126.com>
2017-02-04 09:53:15 +0800

    improve the security config

--- sysMgmt/src/main/java/com/jarvis/SysMgmtApplication.java
+++ sysMgmt/src/main/java/com/jarvis/SysMgmtApplication.java
@@ -3,6 +3,7 @@ package com.jarvis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SysMgmtApplication {
     public static void main(String[] args) {
