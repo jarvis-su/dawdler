9d96592702fee49333a960aadf3f87494130eaac
jarvis-su001 <jarvis_su@126.com>
2017-02-06 21:09:03 +0800

    modify the entities files

--- sysMgmt/src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO Reader (username, password, fullname) VALUES ('craig', 'password', 'Craig Walls');
-INSERT INTO Reader (username, password, fullname) VALUES ('walt', 'marceline', 'Walt Disney');
