81edfd3183fc694377a3a87dfcbe1ccfb45a3340
jarvis-su001 <jarvis_su@126.com>
2017-03-21 10:01:39 +0800

    te

---
+++ test/src/main/java/jarvis/metrics/TestGauges.java
@@ -0,0 +1,12 @@
+package jarvis.metrics;
+
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.TimeUnit;
+
+public class TestGauges {
+	
+	
+	
+
+}
---
+++ test/src/main/java/jarvis/metrics/package-info.java
@@ -0,0 +1,1 @@
+package jarvis.metrics;
