c070eddfda8bf01ec697fc52694838b8ad923367
JarvisSu <jarvis.su@189.cn>
2017-04-17 23:32:15 +0800

    update for hbase demo project

--- HBase-demo/src/main/java/jarvis/CreateTables.java
+++ HBase-demo/src/main/java/jarvis/CreateTables.java
@@ -5,8 +5,11 @@ import org.apache.hadoop.hbase.HBaseConfiguration;
 
 public class CreateTables {
 
+
 	public static void main(String[] args) {
 		Configuration configuration = HBaseConfiguration.create();
+        configuration.set("hbase.zookeeper.quorum", "192.168.31.103");
+        configuration.set("hbase.zookeeper.property.clientPort", "2181");
 
 	}
 
