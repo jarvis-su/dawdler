d53e3d45a77ce6f0872c2f8b5d8a152662ce2c10
jarvis-su001 <jarvis_su@126.com>
2017-04-17 16:50:07 +0800

    add project for hbase demo

---
+++ HBase-demo/src/main/java/jarvis/CreateTables.java
@@ -0,0 +1,13 @@
+package jarvis;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+
+public class CreateTables {
+
+	public static void main(String[] args) {
+		Configuration configuration = HBaseConfiguration.create();
+
+	}
+
+}
