b8974d687f9f6d769297c506f232eadcea85664c
JarvisSu <jarvis.su@189.cn>
2017-04-24 23:51:21 +0800

    update for hadoop project

--- hadoop-demo/src/main/java/jarvis/WordCount.java
+++ hadoop-demo/src/main/java/jarvis/WordCount.java
@@ -9,69 +9,71 @@ import java.io.IOException;
 import java.util.Iterator;
 import java.util.StringTokenizer;
 
+/*
+* -DHADOOP_USER_NAME=jarvis
+* */
 public class WordCount {
-	public static void main(String[] args) throws Exception {
-		String input = "hdfs://192.168.31.103:9000/user/jarvis/input";
-		String output = "hdfs://192.168.31.103:9000/user/jarvis/output/result";
+    public static void main(String[] args) throws Exception {
+        String input = "hdfs://192.168.31.103:9000/user/jarvis/input";
+        String output = "hdfs://192.168.31.103:9000/user/jarvis/output/result";
 
-		JobConf conf = new JobConf(WordCount.class);
-		conf.setUser("jarvis");
+        JobConf conf = new JobConf(WordCount.class);
 
-		conf.setJobName("WordCount");
-		conf.addResource("classpath:/hadoop/core-site.xml");
-		conf.addResource("classpath:/hadoop/hdfs-site.xml");
-		conf.addResource("classpath:/hadoop/mapred-site.xml");
+        conf.setJobName("WordCount");
+        conf.addResource("classpath:/hadoop/core-site.xml");
+        conf.addResource("classpath:/hadoop/hdfs-site.xml");
+        conf.addResource("classpath:/hadoop/mapred-site.xml");
 
 
-		conf.setOutputKeyClass(Text.class);
-		conf.setOutputValueClass(IntWritable.class);
+        conf.setOutputKeyClass(Text.class);
+        conf.setOutputValueClass(IntWritable.class);
 
-		conf.setMapperClass(WordCountMapper.class);
-		conf.setCombinerClass(WordCountReducer.class);
-		conf.setReducerClass(WordCountReducer.class);
+        conf.setMapperClass(WordCountMapper.class);
+        conf.setCombinerClass(WordCountReducer.class);
+        conf.setReducerClass(WordCountReducer.class);
 
-		conf.setInputFormat(TextInputFormat.class);
-		conf.setOutputFormat(TextOutputFormat.class);
+        conf.setInputFormat(TextInputFormat.class);
+        conf.setOutputFormat(TextOutputFormat.class);
 
-		FileInputFormat.setInputPaths(conf, new Path(input));
-		FileOutputFormat.setOutputPath(conf, new Path(output));
+        FileInputFormat.setInputPaths(conf, new Path(input));
+        FileOutputFormat.setOutputPath(conf, new Path(output));
 
-		JobClient.runJob(conf);
-		System.exit(0);
-	}
+        JobClient.runJob(conf);
+        System.exit(0);
+    }
 
-	public static class WordCountMapper extends MapReduceBase implements
-			Mapper<Object, Text, Text, IntWritable> {
-		private final static IntWritable one = new IntWritable(1);
-		private Text word = new Text();
+    public static class WordCountMapper extends MapReduceBase implements
+            Mapper<Object, Text, Text, IntWritable> {
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
 
-		@Override
-		public void map(Object key, Text value,
-				OutputCollector<Text, IntWritable> output, Reporter reporter)
-				throws IOException {
-			StringTokenizer itr = new StringTokenizer(value.toString());
-			while (itr.hasMoreTokens()) {
-				word.set(itr.nextToken());
-				output.collect(word, one);
-			}
-		}
-	}
+        @Override
+        public void map(Object key, Text value,
+                        OutputCollector<Text, IntWritable> output, Reporter reporter)
+                throws IOException {
+            StringTokenizer itr = new StringTokenizer(value.toString());
+            while (itr.hasMoreTokens()) {
+                word.set(itr.nextToken());
+                output.collect(word, one);
+            }
+        }
+    }
 
-	public static class WordCountReducer extends MapReduceBase implements
-			Reducer<Text, IntWritable, Text, IntWritable> {
-		private IntWritable result = new IntWritable();
+    public static class WordCountReducer extends MapReduceBase implements
+            Reducer<Text, IntWritable, Text, IntWritable> {
+        private IntWritable result = new IntWritable();
 
-		@Override
-		public void reduce(Text key, Iterator<IntWritable> values,
-				OutputCollector<Text, IntWritable> output, Reporter reporter)
-				throws IOException {
-			int sum = 0;
-			while (values.hasNext()) {
-				sum += values.next().get();
-			}
-			result.set(sum);
-			output.collect(key, result);
-		}
-	}
+        @Override
+        public void reduce(Text key, Iterator<IntWritable> values,
+                           OutputCollector<Text, IntWritable> output, Reporter reporter)
+                throws IOException {
+            int sum = 0;
+            while (values.hasNext()) {
+                sum += values.next().get();
+            }
+            result.set(sum);
+            output.collect(key, result);
+        }
+    }
 
 }
