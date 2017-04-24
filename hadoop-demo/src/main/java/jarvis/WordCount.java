a114051f7a2e9b65381f3619895bf0239531437f
JarvisSu <jarvis.su@189.cn>
2017-04-24 23:12:50 +0800

    update for hbase demo project

--- hadoop-demo/src/main/java/jarvis/WordCount.java
+++ hadoop-demo/src/main/java/jarvis/WordCount.java
@@ -1,25 +1,45 @@
 package jarvis;
 
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.*;
+
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.StringTokenizer;
 
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.FileInputFormat;
-import org.apache.hadoop.mapred.FileOutputFormat;
-import org.apache.hadoop.mapred.JobClient;
-import org.apache.hadoop.mapred.JobConf;
-import org.apache.hadoop.mapred.MapReduceBase;
-import org.apache.hadoop.mapred.Mapper;
-import org.apache.hadoop.mapred.OutputCollector;
-import org.apache.hadoop.mapred.Reducer;
-import org.apache.hadoop.mapred.Reporter;
-import org.apache.hadoop.mapred.TextInputFormat;
-import org.apache.hadoop.mapred.TextOutputFormat;
-
 public class WordCount {
+	public static void main(String[] args) throws Exception {
+		String input = "hdfs://192.168.31.103:9000/user/jarvis/input";
+		String output = "hdfs://192.168.31.103:9000/user/jarvis/output/result";
+
+		JobConf conf = new JobConf(WordCount.class);
+		conf.setUser("jarvis");
+
+		conf.setJobName("WordCount");
+		conf.addResource("classpath:/hadoop/core-site.xml");
+		conf.addResource("classpath:/hadoop/hdfs-site.xml");
+		conf.addResource("classpath:/hadoop/mapred-site.xml");
+
+
+		conf.setOutputKeyClass(Text.class);
+		conf.setOutputValueClass(IntWritable.class);
+
+		conf.setMapperClass(WordCountMapper.class);
+		conf.setCombinerClass(WordCountReducer.class);
+		conf.setReducerClass(WordCountReducer.class);
+
+		conf.setInputFormat(TextInputFormat.class);
+		conf.setOutputFormat(TextOutputFormat.class);
+
+		FileInputFormat.setInputPaths(conf, new Path(input));
+		FileOutputFormat.setOutputPath(conf, new Path(output));
+
+		JobClient.runJob(conf);
+		System.exit(0);
+	}
+
 	public static class WordCountMapper extends MapReduceBase implements
 			Mapper<Object, Text, Text, IntWritable> {
 		private final static IntWritable one = new IntWritable(1);
@@ -52,33 +72,6 @@ public class WordCount {
 			result.set(sum);
 			output.collect(key, result);
 		}
-	}
-
-	public static void main(String[] args) throws Exception {
-		String input = "hdfs://10.237.88.79:9000/user/jarvis/input";
-		String output = "hdfs://10.237.88.79:9000/user/jarvis/output/result";
-
-		JobConf conf = new JobConf(WordCount.class);
-		conf.setJobName("WordCount");
-		conf.addResource("classpath:/hadoop/core-site.xml");
-		conf.addResource("classpath:/hadoop/hdfs-site.xml");
-		conf.addResource("classpath:/hadoop/mapred-site.xml");
-
-		conf.setOutputKeyClass(Text.class);
-		conf.setOutputValueClass(IntWritable.class);
-
-		conf.setMapperClass(WordCountMapper.class);
-		conf.setCombinerClass(WordCountReducer.class);
-		conf.setReducerClass(WordCountReducer.class);
-
-		conf.setInputFormat(TextInputFormat.class);
-		conf.setOutputFormat(TextOutputFormat.class);
-
-		FileInputFormat.setInputPaths(conf, new Path(input));
-		FileOutputFormat.setOutputPath(conf, new Path(output));
-
-		JobClient.runJob(conf);
-		System.exit(0);
 	}
 
 }
