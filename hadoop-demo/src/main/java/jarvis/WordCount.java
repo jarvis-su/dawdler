35d0a7e387a56399795b62024e0d8c0b69e69544
JarvisSu <jarvis.su@189.cn>
2017-07-18 14:59:39 +0800

    update for hadoop project

--- hadoop-demo/src/main/java/jarvis/WordCount.java
+++ hadoop-demo/src/main/java/jarvis/WordCount.java
@@ -2,9 +2,10 @@ package jarvis;
 
 import java.io.IOException;
 import java.util.Iterator;
-import java.util.Properties;
+
 import java.util.StringTokenizer;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
@@ -22,77 +23,84 @@ import org.apache.hadoop.mapred.TextOutputFormat;
 
 /*
 * -DHADOOP_USER_NAME=jarvis
+* -DHADOOP_USER_NAME=jarvis -Dhadoop.server.ip=192.168.31.103
 * */
 public class WordCount {
 
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-		String envUser = System.getenv("HADOOP_USER_NAME");
-		if (envUser == null) {
-			envUser = System.getProperty("HADOOP_USER_NAME");
-		}
-		Properties ps= System.getProperties();
-		System.out.println(envUser);
+        String envUser = System.getenv("HADOOP_USER_NAME");
+        if (envUser == null) {
+            envUser = System.getProperty("HADOOP_USER_NAME");
+        }
+       // Properties ps = System.getProperties();
+        System.out.println(envUser);
 
-		String input = "hdfs://10.237.88.79:9000/user/jarvis/input";
-		String output = "hdfs://10.237.88.79:9000/user/jarvis/output/result";
+        String hadoopServerIp = System.getProperty("hadoop.server.ip");
+        if (StringUtils.isBlank(hadoopServerIp)) {
+            System.err.println("hadoop.server.ip is empty, user localhost ");
+            hadoopServerIp = "localhost";
+        }
 
-		JobConf conf = new JobConf(WordCount.class);
+        String input = "hdfs://" + hadoopServerIp + ":9000/user/jarvis/input";
+        String output = "hdfs://" + hadoopServerIp + ":9000/user/jarvis/output/result";
 
-		conf.setJobName("WordCount");
-		conf.addResource("classpath:/hadoop/core-site.xml");
-		conf.addResource("classpath:/hadoop/hdfs-site.xml");
-		conf.addResource("classpath:/hadoop/mapred-site.xml");
+        JobConf conf = new JobConf(WordCount.class);
 
-		conf.setOutputKeyClass(Text.class);
-		conf.setOutputValueClass(IntWritable.class);
+        conf.setJobName("WordCount");
+        conf.addResource("classpath:/hadoop/core-site.xml");
+        conf.addResource("classpath:/hadoop/hdfs-site.xml");
+        conf.addResource("classpath:/hadoop/mapred-site.xml");
 
-		conf.setMapperClass(WordCountMapper.class);
-		conf.setCombinerClass(WordCountReducer.class);
-		conf.setReducerClass(WordCountReducer.class);
+        conf.setOutputKeyClass(Text.class);
+        conf.setOutputValueClass(IntWritable.class);
 
-		conf.setInputFormat(TextInputFormat.class);
-		conf.setOutputFormat(TextOutputFormat.class);
+        conf.setMapperClass(WordCountMapper.class);
+        conf.setCombinerClass(WordCountReducer.class);
+        conf.setReducerClass(WordCountReducer.class);
 
-		FileInputFormat.setInputPaths(conf, new Path(input));
-		FileOutputFormat.setOutputPath(conf, new Path(output));
+        conf.setInputFormat(TextInputFormat.class);
+        conf.setOutputFormat(TextOutputFormat.class);
 
-		JobClient.runJob(conf);
-		System.exit(0);
-	}
+        FileInputFormat.setInputPaths(conf, new Path(input));
+        FileOutputFormat.setOutputPath(conf, new Path(output));
 
-	public static class WordCountMapper extends MapReduceBase implements
-			Mapper<Object, Text, Text, IntWritable> {
-		private final static IntWritable one = new IntWritable(1);
-		private Text word = new Text();
+        JobClient.runJob(conf);
+        System.exit(0);
+    }
 
-		@Override
-		public void map(Object key, Text value,
-				OutputCollector<Text, IntWritable> output, Reporter reporter)
-				throws IOException {
-			StringTokenizer itr = new StringTokenizer(value.toString());
-			while (itr.hasMoreTokens()) {
-				word.set(itr.nextToken());
-				output.collect(word, one);
-			}
-		}
-	}
+    public static class WordCountMapper extends MapReduceBase implements
+            Mapper<Object, Text, Text, IntWritable> {
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
 
-	public static class WordCountReducer extends MapReduceBase implements
-			Reducer<Text, IntWritable, Text, IntWritable> {
-		private IntWritable result = new IntWritable();
+        @Override
+        public void map(Object key, Text value,
+                        OutputCollector<Text, IntWritable> output, Reporter reporter)
+                throws IOException {
+            StringTokenizer itr = new StringTokenizer(value.toString());
+            while (itr.hasMoreTokens()) {
+                word.set(itr.nextToken());
+                output.collect(word, one);
+            }
+        }
+    }
 
-		@Override
-		public void reduce(Text key, Iterator<IntWritable> values,
-				OutputCollector<Text, IntWritable> output, Reporter reporter)
-				throws IOException {
-			int sum = 0;
-			while (values.hasNext()) {
-				sum += values.next().get();
-			}
-			result.set(sum);
-			output.collect(key, result);
-		}
-	}
+    public static class WordCountReducer extends MapReduceBase implements
+            Reducer<Text, IntWritable, Text, IntWritable> {
+        private IntWritable result = new IntWritable();
+
+        @Override
+        public void reduce(Text key, Iterator<IntWritable> values,
+                           OutputCollector<Text, IntWritable> output, Reporter reporter)
+                throws IOException {
+            int sum = 0;
+            while (values.hasNext()) {
+                sum += values.next().get();
+            }
+            result.set(sum);
+            output.collect(key, result);
+        }
+    }
 
 }
